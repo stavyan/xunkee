a86e155a80f8ecc1a775629f6ff5ae0e1e8db3d1
stav <stav@stavdeMacBook-Air.local>
2019-03-18 23:19:04 +0800

    修改配置文件

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,27 @@ var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API: '"https://tpm.stavtop.club"',
+  // API: '"http://127.0.0.1:8040"',
   zan_path: '"pages/apps/largess/detail?accountId=5686224"',
   client_id: '"2"',
-  client_secret: '"5rblWYdXKfjTu2TN3n4HtxePPvC0BkIEw6135F14"'
+  client_secret: '"KniOVdV0l8GAFT8YFv6qYoUPTnBKHCyQVhFQdNT5"'
 })
+
+// Prod
+// Encryption keys generated successfully.
+// Personal access client created successfully.
+// Client ID: 1
+// Client Secret: PebTGFSHimMQ2tmqC4PGkXhGWoH8vzv993VdD2Dd
+// Password grant client created successfully.
+// Client ID: 2
+// Client Secret: KniOVdV0l8GAFT8YFv6qYoUPTnBKHCyQVhFQdNT5
+
+
+// Dev
+// Encryption keys generated successfully.
+// Personal access client created successfully.
+// Client ID: 3
+// Client Secret: jMA6zuBNTahqCX2Ucwn7ZyZ9pgg7tDK2BtNOpvtn
+// Password grant client created successfully.
+// Client ID: 4
+// Client Secret: VoTjvqdQAGjE2WdvuCB03tK4VhEl8G79VIVx59IR
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,8 @@
 module.exports = {
   NODE_ENV: '"production"',
   API: '"https://tpm.stavtop.club"',
+  // API: '"http://127.0.0.1:8040"',
   zan_path: '"pages/apps/largess/detail?accountId=5686224"',
   client_id: '"2"',
-  client_secret: '"5rblWYdXKfjTu2TN3n4HtxePPvC0BkIEw6135F14"'
+  client_secret: '"KniOVdV0l8GAFT8YFv6qYoUPTnBKHCyQVhFQdNT5"'
 }
